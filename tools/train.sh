a482e57efe27e5bf7a36af0b3fd1a7dfc862f5fa
whu-pzhang <pzhang@whu.edu.cn>
2022-12-26 11:11:35 +0800

    update

--- tools/train.sh
+++ tools/train.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 now=$(date +"%Y%m%d_%H%M%S")
 
-config=configs/pascal.yaml
-labeled_id_path=partitions/pascal/1464/labeled.txt
-unlabeled_id_path=partitions/pascal/1464/unlabeled.txt
-save_path=exp/pascal/1464/unimatch
+# loveda
+config=configs/loveda.yaml
+# labeled_id_path=partitions/loveda/1_4/labeled.txt
+labeled_id_path=partitions/loveda/train.txt
+unlabeled_id_path=partitions/loveda/1_4/unlabeled.txt
+save_path=exp/loveda/fullsup
+
+# deepglobe road
+
 
 mkdir -p $save_path
 
@@ -12,6 +17,6 @@ python -m torch.distributed.launch \
     --nproc_per_node=$1 \
     --master_addr=localhost \
     --master_port=$2 \
-    unimatch.py \
+    supervised.py \
     --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
     --save-path $save_path --port $2 2>&1 | tee $save_path/$now.txt
